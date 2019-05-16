acfe7ab0823c22431d6dae0da594c0cb29d3bfec
Chris Nuernberger <chris@techascent.com>
2019-05-16 14:31:40 -0600

    correct maven coords.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject libpython-clj "0.1-SNAPSHOT"
+(defproject cnuernber/libpython-clj "0.1-SNAPSHOT"
   :description "libpython bindings to the techascent ecosystem"
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "EPL-2.0"
