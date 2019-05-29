ac8d7a8656fcaa5d48c26ca77ca29b8e01c8353b
Chris Nuernberger <chris@techascent.com>
2019-05-29 13:24:28 -0600

    time to solidify some concepts.

---
+++ test/libpython_clj/python_test.clj
@@ -0,0 +1,8 @@
+(ns libpython-clj.python-test
+  (:require [libpython-clj.python :as py]
+            [clojure.test :refer :all]))
+
+(deftest basic-interpreter-interaction
+  (py/initialize!)
+
+  )
