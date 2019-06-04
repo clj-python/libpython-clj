142c0dbc7056d0f5cd1969548a127a119f641c86
Chris Nuernberger <chris@techascent.com>
2019-06-04 15:39:57 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cnuernber/libpython-clj "0.2"
+(defproject cnuernber/libpython-clj "0.3-SNAPSHOT"
   :description "libpython bindings to the techascent ecosystem"
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "EPL-2.0"
