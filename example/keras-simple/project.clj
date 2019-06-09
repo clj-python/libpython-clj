fe472cea64e502ab98ade7d4f2e0cdfab5be833b
Chris Nuernberger <chris@techascent.com>
2019-06-09 09:04:16 -0600

    logging upgrade.

--- example/keras-simple/project.clj
+++ example/keras-simple/project.clj
@@ -4,5 +4,5 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1-beta2"]
-                 [cnuernber/libpython-clj "0.2"]]
+                 [cnuernber/libpython-clj "0.3"]]
   :repl-options {:init-ns keras-simple.core})
