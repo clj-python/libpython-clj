c16595c0d0141c501354518ffe4cb4435fc64d21
Chris Nuernberger <chris@techascent.com>
2019-07-13 10:26:28 -0600

    upgrade to datatype lib that allows 'vec' to be called on a tensor

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cnuernber/libpython-clj "0.15-SNAPSHOT"
+(defproject cnuernber/libpython-clj "0.14-SNAPSHOT"
   :description "libpython bindings to the techascent ecosystem"
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [techascent/tech.datatype "4.0-beta1"]
+                 [techascent/tech.datatype "4.0-beta2"]
                  [camel-snake-kebab "0.4.0"]]
   :repl-options {:init-ns user}
   :java-source-paths ["java"])
