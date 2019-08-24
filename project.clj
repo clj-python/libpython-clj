e088751a2b831199b6ecba3d09c40f6067766f48
Chris Nuernberger <chris@techascent.com>
2019-08-24 12:10:52 -0600

    datatype grows much more repl friendly.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [techascent/tech.datatype "4.5"]
+                 [techascent/tech.datatype "4.9"]
                  [camel-snake-kebab "0.4.0"]]
   :repl-options {:init-ns user}
   :java-source-paths ["java"])
