72339c450466bb8b404a36a3b581736785ebf8a6
Chris Nuernberger <chris@techascent.com>
2019-11-19 14:26:33 -0700

    datatype colorize test.

--- src/libpython_clj/py_modules/numpy.clj
+++ src/libpython_clj/py_modules/numpy.clj
@@ -1,7 +1,10 @@
 (ns libpython-clj.py-modules.numpy
   (:require [libpython-clj.python :as py]
             [libpython-clj.export-module-symbols
-             :refer [export-module-symbols]]))
+             :refer [export-module-symbols]])
+  (:refer-clojure :exclude [test take str sort short require repeat partition
+                            mod min max long load int identity float empty double
+                            conj char cast byte]))
 
 
 (export-module-symbols "numpy")
