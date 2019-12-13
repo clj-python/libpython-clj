16baa60ade062aada66a1245264c1752edda88d6
Jay <jjtolton@gmail.com>
2019-12-13 13:11:59 -0500

    * tools for finding python library paths

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [techascent/tech.datatype "4.56"]
-                 [camel-snake-kebab "0.4.0"]]
+                 [camel-snake-kebab "0.4.0"]
+                 [cheshire "5.9.0"]]
   :repl-options {:init-ns user}
   :java-source-paths ["java"])
