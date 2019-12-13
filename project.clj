e1238e60f8a180ef23cf3b9fc7b9fafa910f6f6b
J.J. Tolton <jjtolton@gmail.com>
2019-12-13 13:16:09 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [camel-snake-kebab "0.4.0"]
                  [techascent/tech.datatype "4.61"]
                  [cheshire "5.9.0"]]
   :repl-options {:init-ns user}
