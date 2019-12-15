8bb02f2d21a8e952c1cee5811141f30684e3ebbf
Jay <jjtolton@gmail.com>
2019-12-15 04:11:13 -0500

    light overhaul; tests added

--- test/libpython_clj/require_python_test.clj
+++ test/libpython_clj/require_python_test.clj
@@ -1,16 +1,17 @@
 (ns libpython-clj.require-python-test
-  (:require [libpython-clj.require :as req :refer [require-python]]
+  (:require [libpython-clj.require :as req :refer [require-python]
+             :reload true]
             [libpython-clj.python :as py]
             [clojure.test :refer :all]))
 
-
-;;Since this test mutates the global environment we have to accept that
-;;it may not always work.
+;; Since this test mutates the global environment we have to accept that
+;; it may not always work.
 
 (require-python '[math
                   :refer :*
                   :exclude [sin cos]
-                  :as pymath])
+                  :as pymath
+                  :reload true])
 
 
 (deftest base-require-test
@@ -21,7 +22,6 @@
     (is (= 10.0 (double (floor 10.1))))
     (is (= pymath (py/import-module "math")))
     (is (thrown? Throwable (require-python '[math :refer [blah]])))))
-
 
 (deftest parse-flags-test
   ;; sanity check
@@ -50,3 +50,69 @@
     (is (= #{:b} (parse-flags #{:a :b} '[:b true :a false])))
     (is (= #{:b} (parse-flags #{:a :b} '[:b :a false])))
     (is (= #{:b} (parse-flags #{:a :b} '[:b :a false :a true])))))
+
+(deftest python-lib-configuration-test
+  (let [python-lib-configuration #'req/python-lib-configuration
+        simple-req               '[csv]
+        simple-spec              (python-lib-configuration simple-req)
+        {:keys [exclude
+                supported-flags
+                current-ns-sym
+                module-name
+                module-name-or-ns
+                reload?
+                no-arglists?
+                etc
+                current-ns
+                this-module
+                python-namespace
+                refer]}          simple-spec
+        csv-module               (py/import-module "csv")]
+
+    ;; no exclusions
+    (is (= #{} exclude))
+    (is (= #{:no-arglists :reload} supported-flags))
+    (is (= 'libpython-clj.require-python-test
+           current-ns-sym
+           (symbol (str current-ns))))
+    (is (= 'csv module-name module-name-or-ns))
+    (is (nil? reload?))
+    (is (nil? no-arglists?))
+    (is (= {} etc))
+    (is (= csv-module this-module)))
+
+
+  (let [python-lib-configuration #'req/python-lib-configuration
+        simple-req               '[requests
+                                   :reload true
+                                   :refer [get]
+                                   :no-arglists]
+        simple-spec              (python-lib-configuration simple-req)
+        {:keys [exclude
+                supported-flags
+                current-ns-sym
+                module-name
+                module-name-or-ns
+                reload?
+                no-arglists?
+                etc
+                current-ns
+                this-module
+                python-namespace
+                refer]}          simple-spec
+        requests-module          (py/import-module "requests")]
+
+    ;; no exclusions
+    (is (= #{} exclude))
+    (is (= #{:no-arglists :reload} supported-flags))
+    (is (= 'libpython-clj.require-python-test
+           current-ns-sym
+           (symbol (str  current-ns))))
+    (is (= 'requests module-name module-name-or-ns))
+    (is (= reload? :reload))
+    (is (= no-arglists? :no-arglists))
+    (is (= { :refer '[get]}) etc)
+    (is (= requests-module this-module))
+    (is (= #{'get} refer))
+    (is (nil? python-namespace))))
+
