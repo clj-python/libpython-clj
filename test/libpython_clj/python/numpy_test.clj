253269c44f729a32eb2c0a8ca71d92fe4680ff16
Chris Nuernberger <chris@techascent.com>
2019-12-22 15:55:45 -0700

    added unit test for basic binary op.

---
+++ test/libpython_clj/python/numpy_test.clj
@@ -0,0 +1,19 @@
+(ns libpython-clj.python.numpy-test
+  (:require [libpython-clj.python :as py]
+            [tech.v2.datatype :as dtype]
+            [tech.v2.datatype.functional :as dfn]
+            [tech.v2.tensor :as dtt]
+            [clojure.test :refer :all]))
+
+(py/initialize!)
+
+
+;;The basic math operations of the dfn namespace must work on numpy objects.
+(deftest basic-numpy
+  (let [np-mod (py/import-module "numpy")
+        test-data [[1 2] [3 4]]
+        np-ary (py/$a np-mod array test-data)
+        tens (dtt/->tensor test-data)]
+    (is (dfn/equals (-> (dfn/- np-ary 4)
+                        (py/as-tensor))
+                    (dfn/- tens 4)))))
