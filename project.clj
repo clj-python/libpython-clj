dc0fcd559e144ffc51c68755f11727b168f0e2dd
Chris Nuernberger <chris@techascent.com>
2020-01-05 13:06:08 -0700

    upgrade dtype-fix binary-search

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [camel-snake-kebab "0.4.0"]
-                 [techascent/tech.datatype "4.68"]
+                 [techascent/tech.datatype "4.69"]
                  [cheshire "5.9.0"]]
   :repl-options {:init-ns user}
   :java-source-paths ["java"])
