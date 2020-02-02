e6be3a9e8fa24f112d0ff13b60da0fbe76f49aee
Chris Nuernberger <chris@techascent.com>
2020-02-02 09:45:13 -0700

    Adding back project.clj for a more complete build system

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject cnuernber/libpython-clj "1.34-SNAPSHOT"
+  :description "libpython bindings to the techascent ecosystem"
+  :url "http://github.com/cnuernber/libpython-clj"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}
+             :uberjar {:aot :all}}
+  :java-source-paths ["java"])
