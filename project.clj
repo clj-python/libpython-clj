374284ef181f640e9dfca8bfa155dd015ce1498f
Chris Nuernberger <chris@techascent.com>
2020-02-02 10:34:51 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cnuernber/libpython-clj "1.36"
+(defproject cnuernber/libpython-clj "1.37-SNAPSHOT"
   :description "libpython bindings to the techascent ecosystem"
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "Eclipse Public License"
