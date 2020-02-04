4ab6846531188d826a74b221b559351363602240
Chris Nuernberger <chris@techascent.com>
2020-02-04 07:09:24 -0700

    Moving back to lein on main branch for now

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-tools-deps "0.4.5"]]
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
-  :profiles {:dev {:lein-tools-deps/config {:resolve-aliases [:test]}}
-             :uberjar {:aot :all}}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [camel-snake-kebab "0.4.0"]
+                 [techascent/tech.datatype "4.74"]
+                 [org.clojure/data.json "0.2.7"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.5"]]}}
   :java-source-paths ["java"])
