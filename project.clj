56814edc05fdd99b1ef453cf86ae1117d8463cac
Jay <jjtolton@gmail.com>
2020-02-07 00:00:47 -0500

    project.clj

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject cnuernber/libpython-clj "1.37-SNAPSHOT"
+  :description "libpython bindings to the techascent ecosystem"
+  :url "http://github.com/cnuernber/libpython-clj"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev {:dependencies [[criterium "0.4.5"]]}}
+  :java-source-paths ["java"]
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  )
