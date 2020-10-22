c5ac8bda3e4c7a1f177e19b6f623f6cf5ba8e4d9
Chris Nuernberger <chris@techascent.com>
2020-10-22 08:12:00 -0600

    bumping version before merge to master

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-python/libpython-clj "1.47-SNAPSHOT"
+(defproject clj-python/libpython-clj "2.00-SNAPSHOT"
   :description "libpython bindings for Clojure"
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "Eclipse Public License"
