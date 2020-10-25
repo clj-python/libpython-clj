91f58f7802e473c122fd7c8e6370a734f5edd90e
Chris Nuernberger <chris@techascent.com>
2020-10-25 13:51:20 -0600

    upgrade dtype and test

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure    "1.10.2-alpha2"]
                  [camel-snake-kebab      "0.4.0"]
-                 [cnuernber/dtype-next   "6.00-alpha-12"]
+                 [cnuernber/dtype-next   "6.00-alpha-13"]
                  [techascent/tech.jna    "4.04"]
                  [org.clojure/data.json  "1.0.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.5"]]}
