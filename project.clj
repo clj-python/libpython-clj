40e64a885e655d49c3e2bec938eacf9676302d05
Chris Nuernberger <chris@techascent.com>
2020-12-03 10:56:36 -0700

    2.00-alpha-6

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-python/libpython-clj "2.00-alpha-6-SNAPSHOT"
+(defproject clj-python/libpython-clj "2.00-alpha-6"
   :description "libpython bindings for Clojure"
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure    "1.10.2-alpha2"]
                  [camel-snake-kebab      "0.4.0"]
-                 [cnuernber/dtype-next   "6.00-beta-1"]
+                 [cnuernber/dtype-next   "6.00-beta-9"]
                  [techascent/tech.jna    "4.04"]
                  [org.clojure/data.json  "1.0.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.5"]]}
