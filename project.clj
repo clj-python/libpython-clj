9e5526270271f84428cb68b430e5ac231dbe246a
Chris Nuernberger <chris@techascent.com>
2020-12-03 10:56:42 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-python/libpython-clj "2.00-alpha-6"
+(defproject clj-python/libpython-clj "2.00-alpha-7-SNAPSHOT"
   :description "libpython bindings for Clojure"
   :url "http://github.com/cnuernber/libpython-clj"
   :license {:name "Eclipse Public License"
