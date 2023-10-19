6cd7a7e4a229fdd0cddb8e4d1e4845ac8ddbd33e
James J. Tolton <jay@toltontechnology.ai>
2023-10-19 02:25:45 -0400

    add py! macro to sugar w/enhanced(?) data transformation capability
    move to sugar, add doto
    
    add string to sugar

--- src/libpython_clj2/sugar.clj
+++ src/libpython_clj2/sugar.clj
@@ -1,5 +1,6 @@
 (ns libpython-clj2.sugar
-  (:require [libpython-clj2.python :as py]))
+  (:require [libpython-clj2.python :as py]
+            clojure.string))
 
 (py/initialize!)
 
@@ -48,3 +49,63 @@ def pyxfn(g, *args, **kwargs):
           ([result input]
            (py/$a q append input)
            (rf result (pynext gx))))))))
+
+
+(defn ^:private handle-pydotdot!
+  ([x form]
+   (if (list? form)
+     (let [form-data (vec form)
+           [instance-member & args] form-data
+           symbol-str (str instance-member)]
+       (cond
+         (= symbol-str "->")
+         (list* '->  x args)
+
+         (= symbol-str "->>")
+         (list* '->> x args)
+
+         (= symbol-str "as->")
+         (let [[$ & args] args]
+           (list* 'as-> x $ args))
+
+         (= symbol-str "some->")
+         (list* 'some-> x args)
+
+         (= symbol-str "some->>")
+         (list* 'some->> x args)
+
+         (= symbol-str "cond->")
+         (list* 'cond-> x args)
+
+         (= symbol-str "cond->>")
+         (list* 'cond->> x args)
+
+         (get  #{"->py" "->python"} symbol-str)
+         (list* #'py/->python x args)
+
+         (= symbol-str "->jvm")
+         (list* #'py/->jvm x args)
+
+         (= symbol-str "doto")
+         (list* 'doto x args)
+
+         (clojure.string/starts-with? symbol-str "-")
+         (list #'py/py.- x (symbol (subs symbol-str 1 (count symbol-str))))
+
+         (clojure.string/starts-with? symbol-str "**")
+         (list* #'py/py** x (symbol (subs symbol-str 2 (count symbol-str))) args)
+
+         (clojure.string/starts-with? symbol-str "*")
+         (list* #'py/py* x (symbol (subs symbol-str 1 (count symbol-str))) args)
+
+         :else ;; assumed to be method invocation
+
+         (list* (into (vector #'py/py. x instance-member) args))))
+     (handle-pydotdot! x (list form))))
+  ([x form & more]
+   (apply handle-pydotdot! (handle-pydotdot! x form) more)))
+
+
+(defmacro py!
+  [x & args]
+  (apply handle-pydotdot! x args))
